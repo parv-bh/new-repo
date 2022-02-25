eb68fa2b90d44fe722ebfb4829b7cc65854f1a98
Parv Bhadra <parv.bhadra@gmail.com>
2022-02-25 09:56:50 +0400

    no msg

---
+++ untitled.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+/// @title untitled
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+contract Name {
+    
+}
